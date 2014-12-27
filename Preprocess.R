3c344d86825c89cf73624c62498872ccee22f4b7
Alexander <a.vandenbulck@gmail.com>
2014-12-27 16:35:30 +0100

    Added prep:squash to 10 cats

--- Preprocess.R
+++ Preprocess.R
@@ -16,30 +16,48 @@ columnsToUse<-
     "C18"
     )
 
+#assert
+isColNotInNames<- !(columnsToUse %in% names(ctrData))
+if(sum(isColNotInNames)) print("!!WRONG COLUMN NAME IN LIST!!")
+
+#################
+# Create formula
+#################
 ctrFormula<-paste0(target,"~",columnsToUse[1])
 for(col in columnsToUse[2:length(columnsToUse)])
   ctrFormula<-paste0(ctrFormula,"+",col)
 
 ctrFormula<-formula(ctrFormula)
 
-#assert
-isColNotInNames<- !(columnsToUse %in% names(ctrData))
-if(sum(isColNotInNames)) print("!!WRONG COLUMN NAME IN LIST!!")
 
-sizeTrain <-1000
-sizeTest <- 10000 
+###############
+# Create train & test
+###############
+sizeTrain <-100000
+sizeTest <- 1000000 
 trainSet<-ctrData[1:sizeTrain,]
 testSet<-ctrData[(sizeTrain+1):(sizeTrain+1+sizeTest),]
 
 #assert
 if(sum(trainSet$id %in% testSet$id) > 0) print(!!"SAME ID IN TRAIN AND TEST!!")
 
+#################
+# Prep trainset
+#################
+for(col in columnsToUse){
+  sortedNames<-names(sort(table(trainSet[,col]),decreasing = T))
+  top10SortedNames<-sortedNames[1:min(10,length(sortedNames))]
+  valueInTop10 <- trainSet[,col] %in% top10SortedNames
+  
+  newColName<-paste0(col,"Prepped")
+  trainSet[,newColName]<-trainSet[,col]
+  trainSet[,!valueInTop10]<-"Other"
+}
 
 ###########################
 # TRAIN MODEL
 ###########################
 model<-glm(formula=ctrFormula,family = binomial(link="logit"),data=trainSet,control = list(maxit = 100),x=F,y=F)
-
 ###########################
 # TEST MODEL
 ###########################
@@ -49,10 +67,12 @@ for(col in columnsToUse) {
   testSet[isNewValue,col]<-NA
   print(paste0(col," NA:", sum(isNewValue)))
 }
-predictions<-predict(model,testSet)
+
+#predictions<-predict(model,testSet,type="response")
+predictions<-predict(model,testSet,type="response",na.action = na.pass)
 predictions[is.na(predictions)]<-0.17
 
-
+llfun(actual = testSet$clickNum,prediction = predictions)
 
 
 
