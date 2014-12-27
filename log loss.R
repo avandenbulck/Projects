b150365e9c2d969d1c861af31eba55a15448e471
Alexander <a.vandenbulck@gmail.com>
2014-12-27 15:47:16 +0100

    Added log loss function

---
+++ log loss.R
@@ -0,0 +1,7 @@
+(llfun <- function(actual, prediction) {
+  epsilon <- .000000000000001
+  yhat <- pmin(pmax(prediction, epsilon), 1-epsilon)
+  logloss <- -mean(actual*log(yhat)
+                   + (1-actual)*log(1 - yhat))
+  return(logloss)
+})
